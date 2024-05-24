69c40d07be8ff0a0042a6e98aed32fffb0f8e18c
Yasin Soleymanpour <YasinSoley2022@gmail.com>
2024-05-24 23:01:21 +0330

    add button styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Button from './components/Button'
+import Container from './components/Container'
+
 function App() {
-    return <h1>Hello Friend</h1>
+    return (
+        <>
+            <h1 className='header'>Hello Friend</h1>
+            <Container>
+                <Button />
+            </Container>
+        </>
+    )
 }
 
 export default App
