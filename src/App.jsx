28debef4e6d0ee96cbd04d6c0cf33c24362e040b
Yasin Soleymanpour <YasinSoley2022@gmail.com>
2024-05-24 23:11:16 +0330

    add InputText styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Button from './components/Button'
 import Container from './components/Container'
+import InputText from './components/InputText'
 
 function App() {
     return (
         <>
             <h1 className='header'>Hello Friend</h1>
             <Container>
+                <InputText />
                 <Button />
             </Container>
         </>
---
+++ src/components/InputText.jsx
@@ -0,0 +1,9 @@
+export default function InputText() {
+    return (
+        <input
+            type='text'
+            className='input-field'
+            placeholder='Enter your Email Address'
+        />
+    )
+}
